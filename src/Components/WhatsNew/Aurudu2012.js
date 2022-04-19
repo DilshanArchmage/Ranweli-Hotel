91b9486055730549a23879457ac97eb793678a53
Dilshan Dissanayake <dilshan@archmage.lk>
2022-04-19 09:16:29 +0530

    inital

--- src/Components/WhatsNew/Aurudu2012.js
+++ src/Components/WhatsNew/Aurudu2012.js
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
-export default function Aurudu2012() {
-  return (
-    <div>Aurudu2012</div>
-  )
+export default function Aurudu() {
+	return <div>Aurudu Awilla</div>;
 }
